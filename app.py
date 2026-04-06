4888b2af0e9050527cb9633a7a1e70c2e6343934
Akshat Saini <craftspear27@gmail.com>
2026-04-06 11:34:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -62,6 +62,10 @@ def predict():
     except Exception as e:
         return render_template("index.html", prediction_text=f"Error: {str(e)}")
 
+@app.route('/form')
+def form():
+    return render_template("form.html")
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 10000))
     app.run(host="0.0.0.0", port=port)
